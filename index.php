934d7c6afea98ab4982e9319073581368eb17de0
Emmanuel Araújo <165857495+emmanuelrz04@users.noreply.github.com>
2026-03-01 17:56:08 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiros passos</title>
+</head>
+<body>
+    <h1><?php 
+    
+print "Olá mundo \u{1f30e}";
+
+    ?>
+    <p>outra coisa apenas</p>
+</body>
+</html>
