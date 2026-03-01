6a98c28f29160f435d911533fc9f59643ee2e7ed
Emmanuel Araújo <165857495+emmanuelrz04@users.noreply.github.com>
2026-03-01 17:57:35 -0300

    Add files via upload

--- ex000/index.php
+++ ex000/index.php
@@ -3,14 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Primeiros passos</title>
+    <title>Dados do servidor</title>
 </head>
 <body>
-    <h1><?php 
+    <h1>dados do servidor</h1>
+    <?php 
+    phpinfo();
     
-print "Olá mundo \u{1f30e}";
-
     ?>
-    <p>outra coisa apenas</p>
 </body>
 </html>
